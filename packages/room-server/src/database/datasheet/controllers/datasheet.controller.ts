0bfc4286c9d1d4b99a1f70966aefb32a4e633422
baiyx <315779865@qq.com>
2023-06-24 06:39:40 +0800

    增加getcellvalue测试方法

--- packages/room-server/src/database/datasheet/controllers/datasheet.controller.ts
+++ packages/room-server/src/database/datasheet/controllers/datasheet.controller.ts
@@ -170,8 +170,8 @@ export class DatasheetController {
   async cachefiltertodatabase(@Param('dstId') dstId: string,@Param('recordId') recordId: string,@Param('fieldId') fieldId: string) {
     await this.datasheetCacheToDbService.cacheFilterToDatabase(dstId, [recordId],[fieldId],false);
   }
-  @Get(['getcellValue/:dstId/:recordId/:fieldId'])
-  async getcellValue(@Param('dstId') dstId: string,@Param('recordId') recordId: string,@Param('fieldId') fieldId: string) {
-    await this.datasheetCacheToDbService.getCellValue(dstId, [recordId],[fieldId]);
+  @Get(['getcellvalue/:dstId/:recordId/:fieldId'])
+  async getcellValue(@Param('dstId') dstId: string,@Param('recordId') recordId: string,@Param('fieldId') fieldId: string):Promise<string|undefined> {
+   return  await this.datasheetCacheToDbService.getCellValue(dstId, [recordId],[fieldId]);
   }
 }
